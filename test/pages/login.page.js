5459c17eaf032e0762fff55edd32e72f4b1721a0
Phil <knamustarsunder@gmail.com>
2021-06-13 18:20:00 -0400

    updated tests, all current ones work
    Tried to improve some tests for navigating away from the login page, but was unsuccessful due to lack of knowledge in window navigation w/ webdriver /selenium. Will attempt to learn more later, but suffice with simple tests for presence of necessary links for now.

--- test/pages/login.page.js
+++ test/pages/login.page.js
@@ -2,36 +2,114 @@ export function open() {
   return browser.url("https://app.notarize.com/login");
 }
 
-export function verifyTitle() {
+export function verifyLoginTitle() {
   browser.url("https://app.notarize.com/login");
   expect(browser).toHaveTitle("Notarize | Login");
 }
 
-export function verifyHomeButton(){
-  let homeButton = $("a.YwjbjG5cqFpP4Hlhp4Zjv");
-  expect(homeButton).toHaveHref('https://www.notarize.com/');
+export function verifyHomeBtn(){
+  browser.url("https://app.notarize.com/login");
+  let homeBtn = $("a.YwjbjG5cqFpP4Hlhp4Zjv");
+  expect(homeBtn).toHaveHref('https://www.notarize.com/');
+  /*The intention here is to check the new tab opened upon clicking
+    the home button, however I was not familiar enough with webdriver and selenium
+    to figure out how to access the tab in reasonable time. I had thought at first,
+    that there may be a race condition when getting window handles, thus the
+    implementation of the promise */
+  // homeBtn.click();
+  // const myPromise = new Promise((resolve, reject) => {
+  //   resolve(browser.getWindowHandles());
+  // });
+  
+  // myPromise.then((resolve)=>{browser.switchWindow(resolve[1]);})
+  //   .then(()=>{console.log("WE DID IT");})
+  //   .then(()=>{expect(browser).toHaveUrl('https://www.notarize.com/');})
+  //   .then(()=>{browser.closeWindow()})
+  //   .catch(console.log("FAILED"));
+}
+
+export function verifySignUpLink(){
+  let signUpLink = $("a._291g2aF4hfA0TG1L1tmKiS");
+  signUpLink.click();
+  expect(browser).toHaveUrl('https://www.notarize.com/pricing');
+  browser.url("https://app.notarize.com/login");
+}
+
+export function verifyGoogleSignIn(){
+  let googleBtn = $("#google-signin-button");
+  expect(googleBtn).toExist();
+  /*similarly to verifying the home button functionality, I was not familiar enough
+    with selenium or webdriver to figure out how to switch to the new window to 
+    verify that it was the correct google login window*/
+  // googleBtn.click();
+  // expect(browser).toHaveTitle('Sign in - Google Accounts');
+  // browser.closeWindow();
+}
+
+export function verifyNotarizeHelpBtn(){
+  /*having not worked with iframes before, I'm not entirely sure how to test their 
+    functionality. If I have time to return to this I will explore testing this feature
+    further */
+  let helpBtn = $('iframe, #launcher');
+  expect(helpBtn).toExist();
+}
+
+export function noEmailWarn(){
+  browser.url("https://app.notarize.com/login");
+  let emailWarning = $("#field-error-message-id-email");
+  expect(emailWarning).not.toExist();
 }
 
 export function emptyEmailAddr(){
   let emailBox = $("._2-7En-hQFKdqE8IoOpDvfk");
-  let contBtn = $("input[name='email']");
+  //use all classes to identify button due to lack of unique identifiers
+  let contBtn = $("button[class='CoreButton Button is-large is-fullwidth active-darken hover-darken Button-action']");
   emailBox.clearValue("");
-  browser.pause(1000);
   expect(contBtn).toBeDisabled(); 
 }
 
 export function invalidEmailAddr(){
   let emailBox = $("._2-7En-hQFKdqE8IoOpDvfk");
-  let contBtn = $("input[name='email']");
+  //use all classes to identify button due to lack of unique identifiers
+  let contBtn = $("button[class='CoreButton Button is-large is-fullwidth active-darken hover-darken Button-action']");
   emailBox.setValue("phillip.wellheuser");
-  browser.pause(1000);
   expect(contBtn).toBeDisabled(); 
+}
+
+// **this test attempts to test for the "Email appears to be invalid" message, but I 
+// was unable to replicate this in the automated environment
+export function inavalidEmailWarn(){
+  browser.url("https://app.notarize.com/login");
+  let emailWarning = $("#field-error-message-id-email");
+  let emailBox = $("._2-7En-hQFKdqE8IoOpDvfk");
+  expect(emailWarning).not.toExist();
+  emailBox.setValue("phillip.wellheuser@gmail.com");
+  browser.keys('Tab');
+  browser.keys(['ShiftRight', 'Tab']);
+  browser.keys("Backspace");
+  emailBox.setValue("phillip.wellheuser@gmail.");
+  expect(emailWarning).toHaveText('Email Is Required');
+}
+
+export function requiredEmailWarn(){
+  browser.url("https://app.notarize.com/login");
+  let emailWarning = $("#field-error-message-id-email");
+  let emailBox = $("._2-7En-hQFKdqE8IoOpDvfk");
+  expect(emailWarning).not.toExist();
+  emailBox.setValue("phillip.wellheuser@gmail.com");
+  emailBox.setValue("phillip.wellheuser@gmail.");
+  expect(emailWarning).toHaveText('Email Is Required');
 }
 
 export function validEmailAddr(){
   let emailBox = $("._2-7En-hQFKdqE8IoOpDvfk");
-  let contBtn = $("input[name='email']");
+  //use all classes to identify button due to lack of unique identifiers
+  let contBtn = $("button[class='CoreButton Button is-large is-fullwidth active-darken hover-darken Button-action']");
   emailBox.setValue("phillip.wellheuser@gmail.com");
-  // expect(emailBox).toHaveAttrContaining('')
+  // expect(emailBox).not.toHaveValue();
   expect(contBtn).toBeEnabled(); 
+}
+
+export function validEmailContinue(){
+  
 }
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,14 +1,58 @@
 import * as LoginPage from "../pages/login.page";
 
 describe('Notarize Signer Login Page', () => {
+  checkLoginBasics();
+
+  checkPageLinks();
+
+  // checkLoginInput();
+  
+  // checkLoginFunction();
+
+  
+});
+
+function checkLoginBasics(){
   it('should open the login page', () => {
     LoginPage.open();
   });
+
   it('should have the correct title', () => {
-    LoginPage.verifyTitle();
+    LoginPage.verifyLoginTitle();
   });
+}
+
+function checkPageLinks(){
+  //CHECK ALL LINKS ON PAGE
+  //Home button (image)
   it('should have a button linking to the home page', () => {
-    LoginPage.verifyHomeButton();
+    LoginPage.verifyHomeBtn();
+  });
+  //Sign Up
+  it('should have a link to the sign up page', () =>{
+    LoginPage.verifySignUpLink();
+  });
+  //Sign in With Google
+  it('should have a working google login option link', () =>{
+    LoginPage.verifyGoogleSignIn();
+  });
+  //? help button
+  it('should have a Notarize help button', () => {
+    LoginPage.verifyNotarizeHelpBtn();
+  });
+}
+
+function checkLoginInput(){
+  it('should not display a warning message before typing in email input', () => {
+    LoginPage.noEmailWarn();
+  });
+  // it('should display a warning indicating an invalid email address accordingly', () =>{
+  //   //this should only be the case if a complete email was previously entered, then invalidated
+  //   //only occurs after clicking outside text box
+  //   LoginPage.inavalidEmailWarn();
+  // });
+  it('should display a warning indicating an need for email address accordingly', () =>{
+    LoginPage.requiredEmailWarn();
   });
   it('should not enable continue button for empty email addresses', () => {
     LoginPage.emptyEmailAddr();
@@ -19,6 +63,11 @@ describe('Notarize Signer Login Page', () => {
   it('should enable continue button with a complete email address', () => {
     LoginPage.validEmailAddr();
   });
-});
+  it('should advance to the password page when correct email credentials are provided', () =>{
+    LoginPage.validEmailContinue();
+  });
+}
 
-
+function checkPasswordInput(){
+  // CHECK PASSWORD PAGE
+}
